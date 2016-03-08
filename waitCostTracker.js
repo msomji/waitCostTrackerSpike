f0d4a20900ecb7205383316b17afedef2082df9d
msomji <maisam.somji@gmail.com>
2016-03-08 00:55:31 -0500

    added users, and time elapsed field and logic

--- waitCostTracker.js
+++ waitCostTracker.js
@@ -1,23 +1,43 @@
+TimeData = new Mongo.Collection("timeData");
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+    Template.timers.events({
+        "click .btn-success": function() {
+          currentTime = new Date();
+            if(Meteor.user){
+              TimeData.insert({ 
+                userId: Meteor.userId(),
+                userName: Meteor.user().userName,
+                createdAt: currentTime
+              });
+            }
+          Session.set("currentTime", currentTime);
+          console.log("currentTime: " + currentTime);
+        },
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
+        "click .btn-danger": function() {
+          Meteor.call("updateTimeElapsed", Session.get("currentTime"));
+        }
+    });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
+    Accounts.ui.config({
+    passwordSignupFields: "USERNAME_ONLY"
   });
 }
+
+Meteor.methods({
+  updateTimeElapsed : function(createdAt){
+    findEvent = TimeData.findOne({ "userId": Meteor.userId(), createdAt: createdAt });
+    timeElapsed = new Date() - findEvent.createdAt;
+    TimeData.update({createdAt: createdAt}, {$set: {timeElapsed : timeElapsed}});
+  }
+});
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+    Meteor.startup(function() {
+        // code to run on server at startup
+    });
+
+
 }
+
