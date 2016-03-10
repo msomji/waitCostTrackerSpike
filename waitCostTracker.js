af9a48387b42fa74c7189736faec1badada23afb
msomji <maisam.somji@gmail.com>
2016-03-10 01:50:34 -0500

    minro refactoring

--- waitCostTracker.js
+++ waitCostTracker.js
@@ -3,51 +3,47 @@ TimeData = new Mongo.Collection("timeData");
 if (Meteor.isClient) {
     Template.timers.events({
         "click .btn-success": function() {
-          currentTime = new Date();
-            if(Meteor.user){
-              TimeData.insert({ 
-                userId: Meteor.userId(),
-                userName: Meteor.user().userName,
-                teamName: Meteor.user().profile.team,
-                createdAt: currentTime
-              });
+            currentTime = new Date();
+            if (Meteor.user) {
+                TimeData.insert({
+                    userId: Meteor.userId(),
+                    userName: Meteor.user().userName,
+                    teamName: Meteor.user().profile.team,
+                    createdAt: currentTime
+                });
             }
-          Session.set("currentTime", currentTime);
-          console.log("currentTime: " + currentTime);
+            Session.set("currentTime", currentTime);
+            console.log("currentTime: " + currentTime);
         },
 
         "click .btn-danger": function() {
-          Meteor.call("updateTimeElapsed", Session.get("currentTime"));
-           Session.set("currentTime", null);
+            Meteor.call("updateTimeElapsed", Session.get("currentTime"));
+            Session.set("currentTime", null);
         }
     });
 
     Template.personallyWasted.helpers({
-      totalPersonalWasted: function(){
-        allEntries = TimeData.find().fetch();
-        time = 0;
-        allEntries.forEach(function(event){
-          if (Meteor.userId() === event.userId){
-            time += event.timeElapsed != null ? event.timeElapsed : 0;
-          }
-        })
-        return time / 1000;
-      },
-      totalTeamWasted: function(){
-        allEntries = TimeData.find().fetch();
-        time = 0;
-        allEntries.forEach(function(event){
-          if (Meteor.user().profile.team === event.teamName){
-            time += event.timeElapsed != null ? event.timeElapsed : 0;
-          }
-        })
-        return time / 1000;
-      },
-
-      team: function(){
-        return Meteor.user().profile.team;
-      }
+        totalPersonalWasted: function() {
+          return claculateEntries("userId()", "userId" )
+        },
+        totalTeamWasted: function() {
+          return claculateEntries("profile.team", "teamName")
+        },
+        team: function() {
+            return Meteor.user().profile.team;
+        }
     });
+}
+
+function claculateEntries(pathTofieldOnUser, pathTofieldOnTimeData) {
+    allEntries = TimeData.find().fetch();
+    time = 0;
+    allEntries.forEach(function(event) {
+        if (Meteor.pathTofieldOnUser === event.pathTofieldOnTimeData) {
+            time += event.timeElapsed != null ? event.timeElapsed : 0;
+        }
+    })
+    return time / 1000;
 }
 
 AccountsTemplates.addField({
@@ -58,11 +54,11 @@ AccountsTemplates.addField({
 });
 
 Meteor.methods({
-  updateTimeElapsed : function(createdAt){
-    findEvent = TimeData.findOne({ "userId": Meteor.userId(), createdAt: createdAt });
-    timeElapsed = new Date() - findEvent.createdAt;
-    TimeData.update({createdAt: createdAt}, {$set: {timeElapsed : timeElapsed}});
-  }
+    updateTimeElapsed: function(createdAt) {
+        findEvent = TimeData.findOne({ "userId": Meteor.userId(), createdAt: createdAt });
+        timeElapsed = new Date() - findEvent.createdAt;
+        TimeData.update({ createdAt: createdAt }, { $set: { timeElapsed: timeElapsed } });
+    }
 
 });
 
@@ -71,5 +67,3 @@ if (Meteor.isServer) {
         // code to run on server at startup
     });
 }
-
-
